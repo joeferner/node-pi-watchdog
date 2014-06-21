ef70986fef1e17fb2d9d6b6758bfa5752fca90af
Joe Ferner <joe@fernsroth.com>
2014-06-21 09:43:12 -0400

    update example

--- example.js
+++ example.js
@@ -4,9 +4,11 @@ var piWatchdog = require('./');
 
 console.log(piWatchdog);
 
+console.log('getting timeout');
 piWatchdog.getTimeout(function(err, timeout) {
   console.log('getTimeout', arguments);
 
+  console.log('setting timeout');
   piWatchdog.setTimeout(15, function(err, timeout) {
     console.log('setTimeout', arguments);
 
@@ -15,12 +17,14 @@ piWatchdog.getTimeout(function(err, timeout) {
 });
 
 function sendHeartbeat() {
+  console.log('sending heartbeat')
   piWatchdog.heartbeat(function(err) {
     console.log('heartbeat', arguments);
   });
 }
 
 setTimeout(function() {
+  console.log('disabling watchdog')
   piWatchdog.disable(function(err) {
     console.log('disable', arguments);
     process.exit(0);
