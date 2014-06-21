d2f14d6465229678a15e858e51db8c69f8b8588c
Joe Ferner <joe@fernsroth.com>
2014-06-21 09:39:38 -0400

    update example

--- example.js
+++ example.js
@@ -1,12 +1,6 @@
 'use strict';
 
-var readline = require('readline');
 var piWatchdog = require('./');
-
-var rl = readline.createInterface({
-  input: process.stdin,
-  output: process.stdout
-});
 
 console.log(piWatchdog);
 
@@ -17,28 +11,18 @@ piWatchdog.getTimeout(function(err, timeout) {
     console.log('setTimeout', arguments);
 
     setInterval(sendHeartbeat, 5000);
-
-    prompt();
   });
 });
 
 function sendHeartbeat() {
-  piWatchdog.heartbeat(function(err, timeout) {
+  piWatchdog.heartbeat(function(err) {
     console.log('heartbeat', arguments);
   });
 }
 
-function prompt() {
-  rl.question("Disable Watchdog (yes/no)? ", function(answer) {
-    if (answer == 'no') {
-      console.log('Closing without disabling watchdog.');
-      process.exit(0);
-    } else {
-      console.log('disabling watchdog');
-      piWatchdog.disable(function(err, timeout) {
-        console.log('disable', arguments);
-        process.exit(0);
-      });
-    }
+setTimeout(function() {
+  piWatchdog.disable(function(err) {
+    console.log('disable', arguments);
+    process.exit(0);
   });
-}
+}, 20000);
